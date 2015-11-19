461a4e7b3a3b1796238d8834605e2ac465b3bce0
LUIS ROCHA <luisrocha1993@gmail.com>
2015-11-18 18:28:52 -0800

    fixed a couple of bugs. added prompt for player to press start

--- js/virtualPet.js
+++ js/virtualPet.js
@@ -10,131 +10,132 @@ var funeral = new Audio ('audio/funeral.mp3');
 var loveU = new Audio ('audio/loveu.mp3');
 
 function Pet(thePetName, theFormNumber) {
-	this.age = 0;
-	this.hunger = 0;  // random number between 0 and 4.99
-	this.health = 10;
-	this.happiness = 5;
-	this.petName = thePetName;
-	this.formNumber = theFormNumber;
+    this.age = 0;
+    this.hunger = 0;  // random number between 0 and 4.99
+    this.health = 10;
+    this.happiness = 5;
+    this.petName = thePetName;
+    this.formNumber = theFormNumber;
 
-	this.feed = feed;
-	this.play = play;
-	this.medicate = medicate;
-	this.display = display;
-	this.makeOlder = makeOlder;
-	window.document.forms[theFormNumber].petName.value = thePetName;
-	this.display();
+    this.feed = feed;
+    this.play = play;
+    this.medicate = medicate;
+    this.display = display;
+    this.makeOlder = makeOlder;
+    window.document.forms[theFormNumber].petName.value = thePetName;
+    this.display();
 }
 //// cat actions functions, activated by buttons
 function feed() {
-	this.hunger = this.hunger - 1;
-	if (this.hunger < 0){
-		this.hunger = 0;
-	}
-	this.display();
+    this.hunger = this.hunger - 1;
+    if (this.hunger < 0){
+        this.hunger = 0;
+    }
+    this.display();
 }
 
 function play() {
-	this.happiness = this.happiness + 1;
+    this.happiness = this.happiness + 1;
     if (this.happiness > 10){
-		this.happiness = 10;
-	}
-	this.display();
+        this.happiness = 10;
+    }
+    this.display();
 }
 
 function medicate() {
-	this.health = this.health + 1;
-	if (this.health > 10){
-		this.health = 10;
-	} 
+    this.health = this.health + 1;
+    if (this.health > 10){
+        this.health = 10;
+    } 
     this.display();
 }
 
 ///// displays cat's levels on form as text
 function display() {
-	var theString = "";
+    var theString = "";
 
-	if (this.health < minHealth){
-		theString = this.petName + " IS DEAD!";
+    if (this.health < minHealth){
+        theString = this.petName + " IS DEAD!";
         $('.meowmeow').attr("src", "img/kitty-2.png");
-      	funeral.play();
-	} else if (this.age > 20){
+        funeral.play();
+    } else if (this.age > 20){
         theString = this.petName + " has passed of old age. He lived a happy life.";
           $('.meowmeow').attr("src", "img/kitty-3.png");
-    		this.health = 100;
-    		loveU.play();
+            this.health = 100;
+            loveU.play();
     } else {
-		theString += "Happiness " + parseInt(this.happiness);
-		theString += ".  Hunger: " + parseInt(this.hunger);
-		theString += ".  Health: " + parseInt(this.health);		
-		theString += ".  Age: " + parseInt(this.age);
-		theString += ".";
-	}
+        theString += "Happiness " + parseInt(this.happiness);
+        theString += ".  Hunger: " + parseInt(this.hunger);
+        theString += ".  Health: " + parseInt(this.health);     
+        theString += ".  Age: " + parseInt(this.age);
+        theString += ".";
+    }
 
-	window.document.forms[this.formNumber].petStatus.value = theString;
-}	
-	
+    window.document.forms[this.formNumber].petStatus.value = theString;
+}   
+    
 //// "Progression" of time. Ages cat as timer moves forward.
 function makeOlder() {
 
-	var maxHunger = 5;
-	var goodHappiness = 5; 
+    var maxHunger = 5;
+    var goodHappiness = 5; 
 
-	if (this.health > minHealth){
-			
-		this.age += 1;
-		this.happiness -= Math.random() * 2;
-		this.hunger += Math.random() * 2;
-		if (this.hunger > 2){
-			this.health -= Math.random() * 2;
-			this.happiness -= Math.random() * 2;
-		}
+    if (this.health > minHealth){
+            
+        this.age += 1;
+        this.happiness -= Math.random() * 2;
+        this.hunger += Math.random() * 2;
+        if (this.hunger > 2){
+            this.health -= Math.random() * 2;
+            this.happiness -= Math.random() * 2;
+        }
 
-		if (this.health < 0){
-			this.happiness -= Math.random() * 2;
-		}
+        if (this.health < 0){
+            this.happiness -= Math.random() * 2;
+        }
 
-		if (this.hunger < 0){
-			this.hunger = 0;
-		}
+        if (this.hunger < 0){
+            this.hunger = 0;
+        }
 
-		if (this.happiness < 4){
-			$('.meowmeow').attr("src", "img/kitty-1.png");
-		}
+        if (this.happiness < 4){
+            $('.meowmeow').attr("src", "img/kitty-1.png");
+        }
 
-		if (this.happiness > goodHappiness){
-			this.health += Math.random() *0;
+        if (this.happiness > goodHappiness){
+            this.health += Math.random() *0;
           $('.meowmeow').attr("src", "img/kitty1.png");
-		}
+        }
         if (this.health > 10){
           this.health = 10;
         }
         if (this.happiness> 9){
-        	$('.meowmeow').attr("src", "img/kitty2.png");
+            $('.meowmeow').attr("src", "img/kitty2.png");
           purr.play();
         } 
         if (this.happiness < 0){
-        	this.happiness = 0;
+            this.happiness = 0;
         }
-	}
+    }
 
-	this.display();
+    this.display();
 }
 
 
 
 //// activated on start button click. creates new kitty object and begins timer
 function start() {
-	kitty = new Pet(nameKitty,0);
-	 $('.meowmeow').attr("src", "img/kitty0.png");
-	moveTime();
+    kitty = new Pet(nameKitty,0);
+     $('.meowmeow').attr("src", "img/kitty0.PNG");
+    $(".start").hide();
+    moveTime();
 }
 
 ///Timer which progresses game
 function moveTime() {
-	kitty.makeOlder();
-	theTimeOut = setTimeout("moveTime();", 2000);
-}	
+    kitty.makeOlder();
+    theTimeOut = setTimeout("moveTime();", 3000);
+}   
 
 
 ///Click cat, it meows
